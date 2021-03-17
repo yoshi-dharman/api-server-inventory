9b5d30226715a028ab149fa97cc751e716d90771
yoshi-dharman <yoshi.dharman@ti.ukdw.ac.id>
2021-03-17 16:00:03 +0700

    add emoticon

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const userInventory = require("./inventory.route")
 
 router.get("/", (req, res) => {
-    res.send("<h1 style='text-align:center; padding-top:3rem'>Welcome to my Api Inventory</h1>"+
+    res.send("<h1 style='text-align:center; padding-top:3rem'>Welcome to my Api Inventory ⊂◉‿◉つ</h1>"+
     "<h3 style='text-align:center;'>/inventory to see the data</h3>")
 });
 
